aa9d922bb1ba5ff3dfeaac97b7bb77877c2f52f6
aswinnadh <aswinnadh9803@gmail.com>
2025-04-10 00:34:43 +0530

    production 2

--- app/(root)/credits/page.tsx
+++ app/(root)/credits/page.tsx
@@ -1,4 +1,4 @@
-import { SignedIn } from "@clerk/nextjs/";
+import { SignedIn } from "@clerk/nextjs";
 import { auth } from "@clerk/nextjs/server";
 import Image from "next/image";
 import { redirect } from "next/navigation";
