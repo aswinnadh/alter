992ace04a37f258e9bc69726f27bcdcc372ac37c
aswinnadh <aswinnadh9803@gmail.com>
2025-04-10 01:05:00 +0530

    prod 7

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -8,14 +8,19 @@ import Image from "next/image";
 import Link from "next/link";
 
 interface SearchParamProps {
-  searchParams: { page?: string; query?: string };
+  searchParams?: {
+    page?: string;
+    query?: string;
+  };
 }
+
 
 const Home = async ({ searchParams }: SearchParamProps) => {
   const page = Number(searchParams?.page) || 1;
   const searchQuery = (searchParams?.query as string) || '';
 
   const images = await getAllImages({ page, searchQuery });
+
 
   return (
     <>
