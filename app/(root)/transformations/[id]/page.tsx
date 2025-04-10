7aeb2fb6f8b9bcf2ff06ce5f653d7ee9cf653fe4
aswinnadh <aswinnadh9803@gmail.com>
2025-04-10 12:11:46 +0530

    prod12

--- app/(root)/transformations/[id]/page.tsx
+++ app/(root)/transformations/[id]/page.tsx
@@ -9,10 +9,8 @@ import { getImageById } from "@/lib/actions/image.actions";
 import { getImageSize } from "@/lib/utils";
 import { DeleteConfirmation } from "@/components/shared/DeleteConfirmation";
 
-const ImageDetails = async (props: SearchParamProps) => {
-  const { params } =await props;
+const ImageDetails = async ({ params }: { params: { id: string } }) => {
   const { id } = await params;
-
   const { userId } = await auth();
 
   const image = await getImageById(id);
