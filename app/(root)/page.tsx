211cd2b690a24792a45cf80caa9af82bcc2e699d
aswinnadh <aswinnadh9803@gmail.com>
2025-04-10 16:19:10 +0530

    prod30

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,9 +3,8 @@ import { navLinks } from "@/constants";
 import { getAllImages } from "@/lib/actions/image.actions";
 import Image from "next/image";
 import Link from "next/link";
-
 type SearchParamProps = {
-  params: { id: string; type: TransformationTypeKey };
+  params: Promise<{ id: string; type: TransformationTypeKey }>;
   searchParams: { [key: string]: string | string[] | undefined };
 };
 
