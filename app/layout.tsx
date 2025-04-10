2425d715c61a210f62c5b005e307cb25dd37b942
aswinnadh <aswinnadh9803@gmail.com>
2025-04-10 22:24:23 +0530

    prod 46

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
@@ -10,18 +9,21 @@ const IBMPlex = IBM_Plex_Sans({
   weight: ["400", "500", "600", "700"],
 });
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "Alter",
   description: "AI-powered image generation",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <ClerkProvider  appearance={{ variables: { colorPrimary: "#624cf5" } }}>
+    <ClerkProvider 
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY} 
+      appearance={{ variables: { colorPrimary: "#624cf5" } }}
+    >
       <html lang="en">
         <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
           {children}
