8751d248851715ecc32c969c7ec6de0fd8e00242
aswinnadh <aswinnadh9803@gmail.com>
2025-04-10 13:39:19 +0530

    prod17

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,11 +4,16 @@ import { getAllImages } from "@/lib/actions/image.actions";
 import Image from "next/image";
 import Link from "next/link";
 
-const Home = async ({ searchParams }: SearchParamProps) => {
-  const page = Number(searchParams?.page) || 1;
+type HomePageProps = {
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+
+const Home = async ({ searchParams }: HomePageProps) => {
+  const page = await Number(searchParams?.page) || 1;
   const searchQuery = (searchParams?.query as string) || '';
 
-  const images = await getAllImages({ page, searchQuery})
+  const images = await getAllImages({ page, searchQuery });
   return (
     <>
       <section className="home bg-banner px-4 py-10 sm:py-16">
